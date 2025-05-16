b4ad6cfb1178bcbbcb08007ba6357799308eff76
Second <drewuncle863724lu@gmail.com>
2025-05-16 13:27:20 +0800

    all passed

--- graphql/resolvers/queries/get-all-tasks.ts
+++ graphql/resolvers/queries/get-all-tasks.ts
@@ -1,8 +1,8 @@
 import Task from "@/models/Task";
-import exp from "constants";
+
 
 const getAllTasks = async (_: any, { userId }: any) => {
-    return await Task.find({ userId }); // safer and makes more sense
+    return await Task.find({ userId }); 
 }
 
 
